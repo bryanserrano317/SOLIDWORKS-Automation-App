66ea2b2b5f4f8a46095db527fc24ed3d7cf16932
bryanserranogithub <75232089+bryanserranogithub@users.noreply.github.com>
2022-01-21 12:07:05 -0500

    Update minecraftModel.cs

--- SWAutomationApp/minecraftModel.cs
+++ SWAutomationApp/minecraftModel.cs
@@ -22,23 +22,25 @@ namespace automationapp
 
         public void CreatePart()
         {
-            swApp = GetSolidworksAPI.GetApplication();
-
-
+            swApp = GetSolidworksAPI.GetApplication(); // get instance
+            
+            // Gets SOLIDWORKS ready to design a part rather than build an assembly or create a drawing
             defaultPartTemplate = swApp.GetUserPreferenceStringValue((int)swUserPreferenceStringValue_e.swDefaultTemplatePart);
-
-
+            
+            // Lets SOLIDWORKS know to activate the model 
             swModel = (ModelDoc2)swApp.ActiveDoc;
-
+            
+            // Opens Sketch Design menu 
             swModel.InsertSketch2(true);
-
-
+            
+            // API Call for Convert Entities. This call converts all edges from previous square block to be used for new block
             status = swModel.SketchManager.SketchUseEdge3(false, false);
-
+            
+            // Exit out of the Sketch menu
             swModel.ClearSelection2(true);
-
+            
+            // Extrude Sketch to a 1x1mm cube
             swFeature = ((Feature)(swModel.FeatureManager.FeatureExtrusion2(true, false, false, 0, 0, 0.001, 0.001, false, false, false, false, 1, 1, false, false, false, false, false, true, true, 0, 0, false)));
-
 
         }
 
